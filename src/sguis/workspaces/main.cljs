47268c6867ecb8cef6f9672d472adba33dac2f82
Ian Fernandez <d.ian.b@live.com>
2020-12-17 16:26:34 -0300

    making component inside counter-card

--- src/sguis/workspaces/main.cljs
+++ src/sguis/workspaces/main.cljs
@@ -5,19 +5,14 @@
 
 (defonce init (ws/mount))
 
-
-(defonce click-count (r/atom 0))
-
-(defn state-ful-with-atom []
-  [:<>
-   [:div "I have been clicked " @click-count " times."]
-   [:button  {:on-click #(swap! click-count inc)}
-    "Increase"]
-   [:button {:on-click #(reset! click-count 0)}
-    "Reset"]])
-
 (ws/defcard counter-example-card
-  (let [counter (atom 0)]
+  (let [click-count (r/atom 0)]
     (ct.react/react-card
-     counter
-     (r/as-element [state-ful-with-atom]))))
+     click-count
+     (r/as-element
+      [:<>
+       [:div "I have been clicked " @click-count " times."]
+       [:button  {:on-click #(swap! click-count inc)}
+        "Increase"]
+       [:button {:on-click #(reset! click-count 0)}
+        "Reset"]]))))
