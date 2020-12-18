62df4f6a340e32d7c361042f2492ac92efe00339
Ian Fernandez <d.ian.b@live.com>
2020-12-18 20:40:04 -0300

    adding a way to disable input for return flight

--- src/sguis/workspaces/flight_booker.cljs
+++ src/sguis/workspaces/flight_booker.cljs
@@ -6,14 +6,17 @@
   (r/atom {:book-flight :one-way-flight}))
 
 (defn booker-ui [booker-state]
-  [:div {:style {:padding "1em"}}
-   [:div "Book a flight ✈️"]
-   [:label {:id "book-flight"}
-    [:select {:id        "book-selector"
-              :on-change #(swap! booker-state assoc :book-flight (keyword (.. % -target -value)))}
-     [:option {:value "one-way-flight"} "one-way flight"]
-     [:option {:value "return-flight"} "return flight"]]]
-   [:div [:label {:id "go-flight"} [:input {}]]]
-   [:div [:label {:id "return-flight"} [:input {}]]]
-   [:button "Book"]
-   [:pre (with-out-str (pp/pprint @booker-state))]])
+  (let [{:keys [book-flight]} @booker-state]
+    [:div {:style {:padding "1em"}}
+     [:div "Book a flight ✈️"]
+     [:label {:id "book-flight"}
+      [:select {:id        "book-selector"
+                :on-change #(swap! booker-state assoc :book-flight (keyword (.. % -target -value)))}
+       [:option {:value "one-way-flight"} "one-way flight"]
+       [:option {:value "return-flight"} "return flight"]]]
+     [:div [:label {:id "go-flight"}
+            [:input {}]]]
+     [:div [:label {:id "return-flight"}
+            [:input {:disabled (false? (= :one-way-flight book-flight))}]]]
+     [:button "Book"]
+     [:pre (with-out-str (pp/pprint @booker-state))]]))
