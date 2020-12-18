3b3128db29a632a8d79d08474498193ac0f73fa4
Ian Fernandez <d.ian.b@live.com>
2020-12-18 20:00:02 -0300

    adding print component for a selector

--- src/sguis/workspaces/flight_booker.cljs
+++ src/sguis/workspaces/flight_booker.cljs
@@ -1,8 +1,16 @@
 (ns sguis.workspaces.flight-booker
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [cljs.pprint :as pp]))
 
 (def booker-state
   (r/atom {}))
 
 (defn booker-ui [booker-state]
-  [:div "hi"])
+  [:div {:style {:padding "1em"}}
+   [:div "Book a flight ✈️"]
+   [:label {:for "book-flight"}
+    [:select {:on-change #(swap! booker-state assoc :book-flight (.. % -target -value))
+              :id        "book"}
+     [:option {:value "one-way-flight"} "one-way flight"]
+     [:option {:value "return-flight"} "return"]]]
+   [:pre (with-out-str (pp/pprint @booker-state))]])
