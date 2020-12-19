41b5865331b80c1f5c38cc5355ab3f00e966bfdb
Ian Fernandez <d.ian.b@live.com>
2020-12-19 14:37:00 -0300

    decoupling components

--- src/sguis/workspaces/flight_booker.cljs
+++ src/sguis/workspaces/flight_booker.cljs
@@ -5,18 +5,32 @@
 (def booker-state
   (r/atom {:book-flight :one-way-flight}))
 
-(defn booker-ui [booker-state]
+(defn flight-selector [booker-state]
+  [:label {:id "book-flight"}
+   [:select {:id        "book-selector"
+             :on-change #(swap! booker-state assoc :book-flight (keyword (.. % -target -value)))}
+    [:option {:value "one-way-flight"} "one-way flight"]
+    [:option {:value "return-flight"} "return flight"]]])
+
+(defn go-flight-input [booker-state]
+  [:div
+   [:label {:id "go-flight"}
+    [:input {:type      "text"
+             :on-change #(swap! booker-state assoc :go-flight  (.. % -target -value))}]]])
+
+(defn return-flight-input [booker-state]
   (let [{:keys [book-flight]} @booker-state]
-    [:div {:style {:padding "1em"}}
-     [:div "Book a flight ✈️"]
-     [:label {:id "book-flight"}
-      [:select {:id        "book-selector"
-                :on-change #(swap! booker-state assoc :book-flight (keyword (.. % -target -value)))}
-       [:option {:value "one-way-flight"} "one-way flight"]
-       [:option {:value "return-flight"} "return flight"]]]
-     [:div [:label {:id "go-flight"}
-            [:input {}]]]
-     [:div [:label {:id "return-flight"}
-            [:input {:disabled (false? (= :one-way-flight book-flight))}]]]
-     [:button "Book"]
-     [:pre (with-out-str (pp/pprint @booker-state))]]))
+    [:div
+     [:label {:id "return-flight"}
+      [:input {:type      "text"
+               :on-change #(swap! booker-state assoc :return-flight  (.. % -target -value))
+               :disabled  (false? (= :one-way-flight book-flight))}]]]))
+
+(defn booker-ui [booker-state]
+  [:div {:style {:padding "1em"}}
+   [:div "Book a flight ✈️"]
+   [flight-selector booker-state]
+   [go-flight-input booker-state]
+   [return-flight-input booker-state]
+   [:button "Book"]
+   [:pre (with-out-str (pp/pprint @booker-state))]])
