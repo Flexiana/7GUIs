8e4e29e2b16042d67763da59cb818838a7b4e53d
Ian Fernandez <d.ian.b@live.com>
2020-12-21 10:41:04 -0300

    first try on making progresses bar

--- src/sguis/workspaces/timer.cljs
+++ src/sguis/workspaces/timer.cljs
@@ -7,7 +7,24 @@
 (def timer-state
   (r/atom {}))
 
+(def progress-bar-style
+  {:position      "relative"
+   :height        "20px"
+   :width         "350px"
+   :border-radius "50px"
+   :border        "1px solid #333"})
+
+(def filler-style
+  {:background    "#1DA598"
+   :height        "100%"
+   :border-radius "inherit"
+   :transition    "width .2s ease-in"})
+
 (defn timer-ui [timer-state]
   [:div {:style {:padding "1em"}}
    [:div "Timer ⏲️"]
+   [:div {:className "progress-bar"
+          :style     progress-bar-style}
+    [:div {:className "filler"
+           :style     filler-style}]]
    [:pre (with-out-str (pp/pprint @timer-state))]])
