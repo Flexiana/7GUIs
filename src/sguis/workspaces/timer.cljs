37d19a04de00fbc92c8abc0f4134c65566d58bbf
Ian Fernandez <d.ian.b@live.com>
2020-12-21 17:55:41 -0300

    progress-bar working with slider

--- src/sguis/workspaces/timer.cljs
+++ src/sguis/workspaces/timer.cljs
@@ -5,26 +5,38 @@
 
 
 (def *timer
-  (r/atom {}))
+  (r/atom {:timer-slider "50"}))
 
-(def progress-bar-style
+(def container-style
   {:position      "relative"
-   :height        "20px"
-   :width         "350px"
+   :width         "100%"
+   :height        "0.5em"
    :border-radius "50px"
+   :padding       0
    :border        "1px solid #333"})
 
 (def filler-style
-  {:background    "#1DA598"
-   :height        "100%"
-   :border-radius "inherit"
-   :transition    "width .2s ease-in"})
+  {:position "relative"
+   :border-radius "50px"
+   :box-shadow "0 0 10px 0"
+   :height "0.5em"
+   :opacity 1
+   :color "rgb(178, 34, 34)"
+   :background-color "rgb(178, 34, 34)"})
+
+(defn progress-bar
+  [{:keys [timer-slider]}]
+  [:div {:style container-style}
+   [:div {:style (merge filler-style {:width (-> timer-slider
+                                                 (str "%"))})}]])
 
 (defn timer-ui [timer-state]
   [:div {:style {:padding "1em"}}
    [:div "Timer ⏲️"]
-   [:div {:className "progress-bar"
-          :style     progress-bar-style}
-    [:div {:className "filler"
-           :style     filler-style}]]
-   [:pre (with-out-str (pp/pprint @timer-state))]])
+   [progress-bar @*timer]
+   [:div {:class "timer-slider"}
+    [:input {:type "range"
+             :min "1"
+             :max "100"
+             :on-input #(swap! *timer assoc :timer-slider (.. % -target -value))}]]
+   [:pre (with-out-str (pp/pprint @*timer))]])
