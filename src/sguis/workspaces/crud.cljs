c440c5d0ae353af7a444671b6c6f0318c7450db0
Ian Fernandez <d.ian.b@live.com>
2020-12-22 16:15:58 -0300

    refac decoupling components

--- src/sguis/workspaces/crud.cljs
+++ src/sguis/workspaces/crud.cljs
@@ -4,37 +4,55 @@
 (def *crud
   (r/atom {}))
 
+(defn read-component [crud-state]
+  [:span
+   [:label "Name:"]
+   [:input {:type "text"}]]
+  [:span
+   [:label "Lastname:"]
+   [:input {:type "text"}]])
+
+(defn filter-prefix [crud-state]
+  [:div {:style {:margin-bottom "50px"}}
+   [:label "Filter prefix"]
+   [:input {:type "text"}]])
+
+(defn create-person [crud-state]
+  [:button {:style {:margin-right "20px"
+                    :cursor       "pointer"}}
+   "create"])
+
+(defn update-person [crud-state]
+  [:button {:style {:margin-right "20px"
+                    :cursor       "pointer"}}
+   "update"])
+
+(defn delete-person [crud-state]
+  [:button{:style {:margin-right "20px"
+                   :cursor       "pointer"}}
+   "delete"])
+
+(defn people-ui [crud-state]
+  [:div {:style {:display         "flex"
+                 :justify-content "space-between"
+                 :width           "600px"
+                 :margin-bottom   "50px"}}
+   [:div {:style {:width  "45%"
+                  :height "200px"
+                  :border "1px solid gray"}}
+    "name lastname"]
+   [:div {:style {:display        "flex"
+                  :flex-direction "column"
+                  :width          "45%"}}
+    [read-component crud-state]]])
+
 (defn crud-ui [crud-state]
   [:div {:style {:display         "flex"
                  :flex-direction  "column"
                  :justify-content "space-between"}}
-   [:div {:style {:margin-bottom "50px"}}
-    [:lable "Filter pefix"]
-    [:input {:type "text"}]]
-   [:div {:style {:display         "flex"
-                  :justify-content "space-between"
-                  :width           "600px"
-                  :margin-bottom   "50px"}}
-    [:div {:style {:width  "45%"
-                   :height "200px"
-                   :border "1px solid gray"}}
-     "name lastname"]
-    [:div {:style {:display        "flex"
-                   :flex-direction "column"
-                   :width          "45%"}}
-     [:span
-      [:label "Name:"]
-      [:input {:type "text"}]]
-     [:span
-      [:label "Lastname:"]
-      [:input {:type "text"}]]]]
+   [filter-prefix crud-state]
+   [people-ui]
    [:div {:style {:display "flex"}}
-    [:button {:style {:margin-right "20px"
-                      :cursor       "pointer"}}
-     "create"]
-    [:button {:style {:margin-right "20px"
-                      :cursor       "pointer"}}
-     "update"]
-    [:button{:style {:margin-right "20px"
-                     :cursor       "pointer"}}
-     "delete"]]])
+    [create-person crud-state]
+    [update-person crud-state]
+    [delete-person crud-state]]])
