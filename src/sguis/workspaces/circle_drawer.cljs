889bb05394a9456584a2659f8aba33f25f5b12a8
Ian Fernandez <d.ian.b@live.com>
2020-12-24 13:03:59 -0300

    click to dismount the drawing

--- src/sguis/workspaces/circle_drawer.cljs
+++ src/sguis/workspaces/circle_drawer.cljs
@@ -4,21 +4,27 @@
 
 (def *circles
   (r/atom {:dom-node     nil
-           :window-width nil}))
+           :window-width nil
+           :drawing      nil}))
 
 (defn draw-canvas-contents [circles-state]
-  (let [{:keys [dom-node]} @circles-state
-        canvas             (.-firstChild dom-node)
-        ctx                (.getContext canvas "2d")
-        w                  (.-clientWidth canvas)
-        h                  (.-clientHeight canvas)]
-    (.beginPath ctx)
-    (.arc ctx 75 75 50 0 (* 2 js/Math.PI) true)
-    (.stroke ctx)))
+  (let [{:keys [dom-node
+                drawing]} @circles-state
+        canvas            (.-firstChild dom-node)
+        ctx               (.getContext canvas "2d")
+        w                 (.-clientWidth canvas)
+        h                 (.-clientHeight canvas)]
+    (if-not drawing
+      (do (.beginPath ctx)
+          (.arc ctx 75 75 50 0 (* 2 js/Math.PI) true)
+          (.stroke ctx)
+          :draw)
+      (.clearRect ctx 0 0 (.-width canvas) (.-height canvas)))))
 
 (defn div-with-canvas [circles-state]
   (let [{:keys [window-widht
-                dom-node]} @circles-state]
+                dom-node
+                drawing]} @circles-state]
     (r/create-class
      {:component-did-mount
       (fn [this]
@@ -28,12 +34,22 @@
       (fn [ ]
         window-widht
         [:div.with-canvas
-         [:canvas {:on-click #(draw-canvas-contents circles-state)}
+         [:canvas {:on-click #(if-not drawing
+                                (swap! circles-state
+                                       assoc
+                                       :drawing
+                                       (draw-canvas-contents circles-state))
+                                (swap! circles-state
+                                       dissoc
+                                       :drawing))}
           (when dom-node
             {:width  (.-clientWidth dom-node)
              :height (.-clientHeight dom-node)})]])})))
 
 
 (defn circles-ui [circles-state]
-  [:<> [:div "HI!"]
-   [div-with-canvas circles-state]])
+  [:<>
+   [:div "HI!"]
+   [:dialog {:id "dialog-try"}]
+   [div-with-canvas circles-state]
+   [:pre (with-out-str (cljs.pprint/pprint @*circles))]])
