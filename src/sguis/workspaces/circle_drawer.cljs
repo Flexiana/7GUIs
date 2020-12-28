ffd5d40485e1fbf25f846cccfef2872614c1351d
Ian Fernandez <d.ian.b@live.com>
2020-12-28 17:14:16 -0300

    I can draw circles when click on canvas :smile:

--- src/sguis/workspaces/circle_drawer.cljs
+++ src/sguis/workspaces/circle_drawer.cljs
@@ -3,68 +3,54 @@
             [reagent.dom :as dom]))
 
 (def *circles
-  (r/atom {:dom-node     nil
-           :window-width nil
+  (r/atom {:window-width nil
            :drawing      nil}))
 
 
-
-(defn ui-draw-circles-on-canvas [circles-state]
-  (let [{:keys [dom-node
+(defn ui-draw-circles-on-canvas [circles-state mouse-event]
+  (let [{:keys [canvas
                 drawing]} @circles-state
-        canvas            (.-firstChild dom-node)
-        ctx               (.getContext canvas "2d")
-        w                 (.-clientWidth canvas)
-        h                 (.-clientHeight canvas)]
+        rect              (.getBoundingClientRect canvas)
+        xrel              (- (.-clientX mouse-event) (.-left rect) )
+        yrel              (- (.-clientY mouse-event) (.-top rect))
+        ctx               (.getContext canvas "2d")]
     (if-not drawing
       (do (doto ctx
             (.beginPath)
-            (.arc 75 75 50 0 (* 2 Math/PI)
-                  )
+            (.arc xrel yrel 50 0 (* 2 Math/PI))
             ;;    x  y  r  startangle endangle
             (.stroke))
           :draw)
       (do (swap! circles-state dissoc :drawing)
           (.clearRect ctx 0 0 (.-width canvas) (.-height canvas))))))
 
-;;(defn dialog [state id] (get-in state [:dialogs id]))
-
-
-;; (let [{:keys [opened?] :as dialog-state} (dialog @state)] (when opened? [dialog dialog-state :id-1]))
-
-#_{:dialogs
-   {:id-1
-    {:opened? false}}
-   :id-2
-   {:opened? true}}
-
 (defn div-with-canvas [circles-state]
   (let [{:keys [window-widht
-                dom-node
+                canvas
                 drawing]} @circles-state]
-
     (r/create-class
      {:component-did-mount
       (fn [this]
-        (swap! circles-state assoc :dom-node (dom/dom-node this)))
-
+        (swap! circles-state assoc :canvas (-> this
+                                               dom/dom-node
+                                               .-firstChild)))
       :reagent-render
-      (fn [ ]
-        window-widht
+      (fn []
         [:div.with-canvas
-         [:canvas {:style           {:border "1px solid #000000"}
-                   :on-click        #(swap! circles-state
-                                            assoc
-                                            :drawing
-                                            (ui-draw-circles-on-canvas circles-state))
+         [:canvas {:style           {:border   "1px solid #000000"
+                                     :position "relative"}
+                   :on-click        (fn [mouse-event]
+                                      (swap! circles-state
+                                             assoc
+                                             :drawing
+                                             (ui-draw-circles-on-canvas circles-state mouse-event)))
                    :on-context-menu (fn [event]
-                                      (js/console.log event)
                                       (swap! circles-state assoc :modal-opened true)
                                       (when event
                                         (.preventDefault event)))}
-          (when dom-node
-            {:width  (.-clientWidth dom-node)
-             :height (.-clientHeight dom-node)})]])})))
+          (when canvas
+            {:width  (.-clientWidth canvas)
+             :height (.-clientHeight canvas)})]])})))
 
 (defn input-circles-ui [circles-state]
   (let [{:keys [modal-opened]} @circles-state]
@@ -73,11 +59,10 @@
       [:div])))
 
 (defn circles-ui [circles-state]
-  [:<>
+  [:div {:padding "1em"}
    [:div "HI!"]
    [:div
     [:button "Undo"]
     [:button "Redo"]]
-   [:div
-    [div-with-canvas circles-state]]
+   [:div [div-with-canvas circles-state]]
    [:pre (with-out-str (cljs.pprint/pprint @*circles))]])
