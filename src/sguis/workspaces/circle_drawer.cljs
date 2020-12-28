4375cba5e76a08c06b873b686baad490589741a8
Ian Fernandez <d.ian.b@live.com>
2020-12-28 14:20:14 -0300

    on-context-menu deactivated

--- src/sguis/workspaces/circle_drawer.cljs
+++ src/sguis/workspaces/circle_drawer.cljs
@@ -7,7 +7,9 @@
            :window-width nil
            :drawing      nil}))
 
-(defn draw-canvas-contents [circles-state]
+
+
+(defn ui-draw-circles-on-canvas [circles-state]
   (let [{:keys [dom-node
                 drawing]} @circles-state
         canvas            (.-firstChild dom-node)
@@ -15,17 +17,32 @@
         w                 (.-clientWidth canvas)
         h                 (.-clientHeight canvas)]
     (if-not drawing
-      (do (.beginPath ctx)
-          (.arc ctx 75 75 50 0 (* 2 js/Math.PI) true)
-          (.stroke ctx)
+      (do (doto ctx
+            (.beginPath)
+            (.arc 75 75 50 0 (* 2 Math/PI)
+                  )
+            ;;    x  y  r  startangle endangle
+            (.stroke))
           :draw)
       (do (swap! circles-state dissoc :drawing)
           (.clearRect ctx 0 0 (.-width canvas) (.-height canvas))))))
+
+;;(defn dialog [state id] (get-in state [:dialogs id]))
+
+
+;; (let [{:keys [opened?] :as dialog-state} (dialog @state)] (when opened? [dialog dialog-state :id-1]))
+
+#_{:dialogs
+   {:id-1
+    {:opened? false}}
+   :id-2
+   {:opened? true}}
 
 (defn div-with-canvas [circles-state]
   (let [{:keys [window-widht
                 dom-node
                 drawing]} @circles-state]
+
     (r/create-class
      {:component-did-mount
       (fn [this]
@@ -35,16 +52,32 @@
       (fn [ ]
         window-widht
         [:div.with-canvas
-         [:canvas {:on-click #(swap! circles-state
-                                     assoc
-                                     :drawing
-                                     (draw-canvas-contents circles-state))}
+         [:canvas {:style           {:border "1px solid #000000"}
+                   :on-click        #(swap! circles-state
+                                            assoc
+                                            :drawing
+                                            (ui-draw-circles-on-canvas circles-state))
+                   :on-context-menu (fn [event]
+                                      (js/console.log event)
+                                      (swap! circles-state assoc :modal-opened true)
+                                      (when event
+                                        (.preventDefault event)))}
           (when dom-node
             {:width  (.-clientWidth dom-node)
              :height (.-clientHeight dom-node)})]])})))
 
+(defn input-circles-ui [circles-state]
+  (let [{:keys [modal-opened]} @circles-state]
+    (if modal-opened
+      [:div (with-out-str (cljs.pprint/pprint (str modal-opened)))]
+      [:div])))
 
 (defn circles-ui [circles-state]
   [:<>
    [:div "HI!"]
-   [div-with-canvas circles-state]])
+   [:div
+    [:button "Undo"]
+    [:button "Redo"]]
+   [:div
+    [div-with-canvas circles-state]]
+   [:pre (with-out-str (cljs.pprint/pprint @*circles))]])
