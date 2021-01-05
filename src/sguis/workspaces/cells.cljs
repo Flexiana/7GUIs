3a76764359a3067559cf255912282225cc1881b8
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-05 17:18:40 -0300

    Fix multi-arity injection to map injection

--- src/sguis/workspaces/cells.cljs
+++ src/sguis/workspaces/cells.cljs
@@ -27,9 +27,8 @@
 (defn change-cell! [*state event]
   (swap! *state assoc :edition (.. event -target -value)))
 
-(defn coll-fn [{:keys [focused-cell
-                       cells
-                       edition]} focus-cell! submit-cell! change-cell! l c]
+(defn coll-fn [{:keys [focused-cell cells edition]}
+               {:keys [focus-cell! submit-cell! change-cell!]} l c]
   (let [cell-id (keyword (str l c))]
     ^{:key cell-id}
     [:td {:style           {:border "1px solid black"}
@@ -41,12 +40,12 @@
                        :on-change change-cell!}]]
              (get cells cell-id))]]))
 
-(defn row-fn [cells-derref focus-cell! submit-cell! change-cell! l]
+(defn row-fn [cells actions-map l]
   ^{:key l}
   [:tr
    (concat [^{:key l}
             [:td {:style {:border "1px solid black"}} l]]
-           (map (partial coll-fn cells-derref focus-cell! submit-cell! change-cell! l) a->z))])
+           (map (partial coll-fn cells actions-map l) a->z))])
 
 (defn cells-ui [*cells]
   [:table {:style {:border "1px solid black"}}
@@ -54,6 +53,6 @@
                    (map header-fn a->z))]
    [:tbody (concat [^{:key :n} [:th]]
                    (map (partial row-fn @*cells
-                                 (partial focus-cell! *cells)
-                                 (partial submit-cell! *cells)
-                                 (partial change-cell! *cells)) table-lines))]])
+                                 {:focus-cell!  (partial focus-cell! *cells)
+                                  :submit-cell! (partial submit-cell! *cells)
+                                  :change-cell! (partial change-cell! *cells)}) table-lines))]])
