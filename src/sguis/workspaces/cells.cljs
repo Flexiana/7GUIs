dfc83fef2e85cb517e6826c205143311819ff2aa
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-05 07:57:36 -0300

    Add a first try on making a table

--- src/sguis/workspaces/cells.cljs
+++ src/sguis/workspaces/cells.cljs
@@ -2,10 +2,40 @@
   (:require [reagent.core :as r]))
 
 (def *cells
-  (r/atom {}))
+  (r/atom {:cells [{:id 0 :x 1 :y 2}]}))
 
 (def a->z
-  (map char (range 97 123)))
+  (map (comp (fn [c] [:td c]) char) (range 65 91)))
 
-(defn cells-ui [cells-state]
-  [:table])
+(defn cells-ui [*cells]
+  (let [{:keys [cells]} @*cells
+        columns         [{:attr  :x
+                          :label "x"}
+                         {:attr  :y
+                          :label "y"}]]
+    (letfn [(row-fn [line {:keys [attr]}]
+              ^{:key attr}
+              [:td {:on-click #(swap! *cells assoc :selected? line)}
+               (get line attr)])
+            (column-fn [columns {:keys [id] :as line}]
+              [:tr
+               [:td]
+               [:td id]
+               (map (partial row-fn line) columns)])]
+      [:table
+       (map (fn [[line coll]]
+              [:<>
+               [:thead (concat [[:th]] a->z)]
+               [:tbody (concat [[:th]]
+                               (map (fn [c]
+                                      [:tr [:td c]
+                                       [:td line]])
+                                    ;; lines
+                                    (range 0 10)))]]) {0 {:A 1 :B 2}})]
+
+      #_[:table
+         [:thead
+          (concat
+           [[:th] [:td "id"] [:td "a"] [:td "b"]])]
+         [:tbody
+          (map (partial column-fn columns) cells)]])))
