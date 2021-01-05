95356967db62572697cd736aa9df504c94570ff5
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-05 16:59:56 -0300

    Decouple actions from coll-fn

--- src/sguis/workspaces/cells.cljs
+++ src/sguis/workspaces/cells.cljs
@@ -16,33 +16,45 @@
   ^{:key c}
   [:td {:style {:border "1px solid black"}} c])
 
-(defn coll-fn [focused-cell cells edition l c]
+(defn focus-cell! [*state cell-id _]
+  (swap! *state assoc :focused-cell cell-id))
+
+
+(defn submit-cell! [*state cell-id edition event]
+  (.preventDefault event)
+  (swap! *state assoc-in [:cells cell-id] edition)
+  (swap! *state dissoc :focused-cell))
+
+(defn change-cell! [*state event]
+  (swap! *state assoc :edition (.. event -target -value)))
+
+(defn coll-fn [{:keys [focused-cell
+                       cells
+                       edition]} focus-cell! submit-cell! change-cell! l c]
   (let [cell-id (keyword (str l c))]
     ^{:key cell-id}
     [:td {:style           {:border "1px solid black"}
-          :on-double-click #(swap! *cells assoc :focused-cell cell-id)}
-     [:div (when (= cell-id focused-cell)
+          :on-double-click (partial focus-cell! cell-id)}
+     [:div (if (= cell-id focused-cell)
              [:form {:id        cell-id
-                     :on-submit #(do (.preventDefault %)
-                                     (swap! *cells assoc-in [:cells cell-id] edition)
-                                     (swap! *cells dissoc :focused-cell))}
+                     :on-submit (partial submit-cell! cell-id edition)}
               [:input {:type      "text"
-                       :on-change #(swap! *cells assoc :edition (.. % -target -value))}]])
-      (get cells cell-id)]]))
+                       :on-change change-cell!}]]
+             (get cells cell-id))]]))
 
-(defn row-fn [focused-cell cells edition l]
+(defn row-fn [cells-derref focus-cell! submit-cell! change-cell! l]
   ^{:key l}
   [:tr
    (concat [^{:key l}
             [:td {:style {:border "1px solid black"}} l]]
-           (map (partial coll-fn focused-cell cells edition l) a->z))])
+           (map (partial coll-fn cells-derref focus-cell! submit-cell! change-cell! l) a->z))])
 
 (defn cells-ui [*cells]
-  (let [{:keys [focused-cell
-                cells
-                edition]} @*cells]
-    [:table {:style {:border "1px solid black"}}
-     [:thead (concat [^{:key :n} [:td]]
-                     (map header-fn  a->z))]
-     [:tbody (concat [^{:key :n} [:th]]
-                     (map (partial row-fn focused-cell cells edition) table-lines))]]))
+  [:table {:style {:border "1px solid black"}}
+   [:thead (concat [^{:key :n} [:td]]
+                   (map header-fn  a->z))]
+   [:tbody (concat [^{:key :n} [:th]]
+                   (map (partial row-fn @*cells
+                                 (partial focus-cell! *cells)
+                                 (partial submit-cell! *cells)
+                                 (partial change-cell! *cells)) table-lines))]])
