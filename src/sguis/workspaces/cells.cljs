be04d798a15b02b1c57540d5f6c5b958196cb52c
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-05 15:04:00 -0300

    Add some borders

--- src/sguis/workspaces/cells.cljs
+++ src/sguis/workspaces/cells.cljs
@@ -13,15 +13,17 @@
 (defn cells-ui [*cells]
   [:table {:style {:border "1px solid black"}}
    [:thead (concat [^{:key :n} [:th]]
-                   (map (fn [c] ^{:key c} [:td c])  a->z))]
+                   (map (fn [c] ^{:key c} [:td {:style {:border "1px solid black"}} c])  a->z))]
    [:tbody (concat [^{:key :n} [:th]]
                    (map (fn [l]
                           ^{:key l}
                           [:tr
                            ;; coll entries per line
-                           (concat [^{:key l} [:td l]]
+                           (concat [^{:key l}
+                                    [:td {:style {:border "1px solid black"}} l]]
                                    (map (fn [c]
                                           ^{:key (str l c)}
-                                          [:td {:on-click #(js/console.log (str l c))} c]) a->z))])
+                                          [:td {:style    {:border "1px solid black"}
+                                                :on-click #(js/console.log (str l c))} ""]) a->z))])
                         ;; lines
                         table-lines))]])
