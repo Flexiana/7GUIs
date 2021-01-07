b7786aaff342c6c0ff3a208f7e37009fa7ffd603
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-07 10:49:43 -0300

    Move and adding comments about Parser Impl and Ui impl

--- src/sguis/workspaces/cells.cljs
+++ src/sguis/workspaces/cells.cljs
@@ -3,6 +3,80 @@
             [reagent.core :as r]
             [sci.core :refer [eval-form]]))
 
+;; Parser Impl
+(defn numeric? [x]
+  (and (number? x) (not (js/Number.isNaN x))))
+
+(def kw->op
+  {:add  #(+ %1 %2)
+   :sub  #(- %1 %2)
+   :div  #(/ %1 %2)
+   :mul  #(* %1 %2)
+   :mod  #(mod %1 %2)
+   :sum  +
+   :prod *})
+
+(defn can-parse-numeric? [parsed-exp]
+  (and (re-find  #"^[0-9]" parsed-exp)
+       (not (re-find #"[aA-zZ]" parsed-exp))
+       (numeric? (js/parseFloat parsed-exp))))
+
+(defn is-cell? [parsed-exp]
+  (and (= 2 (count parsed-exp))
+       (possible-cells (keyword (str/upper-case parsed-exp)))))
+
+(defn is-range-cells? [parsed-exp]
+  (and (= 5 (count parsed-exp))
+       (->> (str/split parsed-exp #":")
+            (map (comp boolean possible-cells keyword str/upper-case))
+            (every? true?))))
+
+(defn range-cells-get [kw-range]
+  (let [[fst snd]     kw-range
+        [collmin min] (name fst)
+        [collmax max] (name snd)]
+    (for [collv (range (.charCodeAt collmin) (inc (.charCodeAt collmax)))
+          v     (range (int min) (inc (int max)))]
+      (keyword (str (char collv) v)))))
+
+(defn is-op? [parsed-exp]
+  ((set (keys kw->op)) (keyword parsed-exp)))
+
+(defn tokenizer [{:keys [cells]} parsed-exp]
+  (cond
+    (can-parse-numeric? parsed-exp) (js/parseFloat parsed-exp)
+    (is-cell? parsed-exp)           (get cells (keyword (str/upper-case parsed-exp)) 0)
+    (is-range-cells? parsed-exp)    (->> (-> parsed-exp
+                                             (str/upper-case)
+                                             (str/split #":")
+                                             range-cells-get)
+                                         (map keyword)
+                                         (map #(get cells % 0)))
+    (is-op? parsed-exp)             (get kw->op (keyword parsed-exp))))
+
+(defn parse [env s]
+  (let [parsed (-> s
+                   str/lower-case)]
+    (cond (str/ends-with? parsed "=") (->> (str/split parsed #" ")
+                                           (map (partial tokenizer env))
+                                           (remove nil?))
+          :else                       s)))
+
+(defn eval-cell [env s]
+  (eval-form {}
+             (flatten (parse env s))))
+
+;; Manual tests
+#_(is (= 10 (eval-cell {:cells {:A2 2 :B8 8}} "Sum of A2:B8 =")))
+#_(is (= `(~+ (0 0 0 0 0 0 0 0 0 0 0 0 0 0)) (parse {} "Sum of A2:B8 ="))
+      (= `(~+ 4.0 (0 0 0 0 0 0 0 0 0 0 0 0 0 0)) (parse {} "Sum of 4 and A2:B8 ="))
+      (= `(~/ 0 0) (parse {} "Div of B5 and C5 ="))
+      (= `(2) (parse {} "2 =")))
+#_(is (= :A3 (tokenizer "A3"))
+      (= [:A3 :B5] (tokenizer "A3:B5"))
+      (= + (tokenize "sum")))
+
+;; UI impl
 (def *cells
   (r/atom {:focused-cell nil
            :edition      ""
@@ -85,74 +159,3 @@
                            {:focus-cell!  (partial focus-cell! *cells)
                             :submit-cell! (partial submit-cell! *cells)
                             :change-cell! (partial change-cell! *cells)}) table-lines))]]])
-
-(defn numeric? [x]
-  (and (number? x) (not (js/Number.isNaN x))))
-
-(def kw->op
-  {:add  #(+ %1 %2)
-   :sub  #(- %1 %2)
-   :div  #(/ %1 %2)
-   :mul  #(* %1 %2)
-   :mod  #(mod %1 %2)
-   :sum  +
-   :prod *})
-
-(defn can-parse-numeric? [parsed-exp]
-  (and (re-find  #"^[0-9]" parsed-exp)
-       (not (re-find #"[aA-zZ]" parsed-exp))
-       (numeric? (js/parseFloat parsed-exp))))
-
-(defn is-cell? [parsed-exp]
-  (and (= 2 (count parsed-exp))
-       (possible-cells (keyword (str/upper-case parsed-exp)))))
-
-(defn is-range-cells? [parsed-exp]
-  (and (= 5 (count parsed-exp))
-       (->> (str/split parsed-exp #":")
-            (map (comp boolean possible-cells keyword str/upper-case))
-            (every? true?))))
-
-(defn range-cells-get [kw-range]
-  (let [[fst snd]     kw-range
-        [collmin min] (name fst)
-        [collmax max] (name snd)]
-    (for [collv (range (.charCodeAt collmin) (inc (.charCodeAt collmax)))
-          v     (range (int min) (inc (int max)))]
-      (keyword (str (char collv) v)))))
-
-(defn is-op? [parsed-exp]
-  ((set (keys kw->op)) (keyword parsed-exp)))
-
-(defn tokenizer [{:keys [cells]} parsed-exp]
-  (cond
-    (can-parse-numeric? parsed-exp) (js/parseFloat parsed-exp)
-    (is-cell? parsed-exp)           (get cells (keyword (str/upper-case parsed-exp)) 0)
-    (is-range-cells? parsed-exp)    (->> (-> parsed-exp
-                                             (str/upper-case)
-                                             (str/split #":")
-                                             range-cells-get)
-                                         (map keyword)
-                                         (map #(get cells % 0)))
-    (is-op? parsed-exp)             (get kw->op (keyword parsed-exp))))
-
-(defn parse [env s]
-  (let [parsed (-> s
-                   str/lower-case)]
-    (cond (str/ends-with? parsed "=") (->> (str/split parsed #" ")
-                                           (map (partial tokenizer env))
-                                           (remove nil?))
-          :else                       s)))
-
-(defn eval-cell [env s]
-  (eval-form {}
-             (flatten (parse env s))))
-
-#_(is (= 10 (eval-cell {:cells {:A2 2 :B8 8}} "Sum of A2:B8 =")))
-#_(is (= `(~+ (0 0 0 0 0 0 0 0 0 0 0 0 0 0)) (parse {} "Sum of A2:B8 ="))
-      (= `(~+ 4.0 (0 0 0 0 0 0 0 0 0 0 0 0 0 0)) (parse {} "Sum of 4 and A2:B8 ="))
-      (= `(~/ 0 0) (parse {} "Div of B5 and C5 ="))
-      (= `(2) (parse {} "2 =")))
-#_(is (= :A3 (tokenizer "A3"))
-      (= [:A3 :B5] (tokenizer "A3:B5"))
-      (= + (tokenize "sum")))
