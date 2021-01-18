18b957ed6dc3305f56d7a3d084e5e5e326ef0fa8
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-18 18:10:31 -0300

    Finish tests for Counter

--- test/sguis/workspaces/counter_test.cljs
+++ test/sguis/workspaces/counter_test.cljs
@@ -1,10 +1,9 @@
 (ns sguis.workspaces.counter-test
-  (:require [sguis.workspaces.counter :refer [counter-ui *counter]]
+  (:require [sguis.workspaces.counter :refer [counter-ui counter-start]]
             [cljs.test :as t
              :include-macros true
              :refer [is testing use-fixtures]]
             [nubank.workspaces.core :as ws]
-            [dommy.core :as dommy :refer-macros [sel1]]
             [reagent.core :as r]
             ["@testing-library/react" :as rtl]))
 
@@ -12,11 +11,29 @@
   {:after rtl/cleanup})
 
 (ws/deftest counter-tests
-  (let [ui (->> [counter-ui *counter]
-                r/as-element
-                rtl/render)]
+  (let [ui               (-> [counter-ui (r/atom counter-start)]
+                             r/as-element
+                             rtl/render)
+        get-counter-elem (fn []
+                           (-> ui
+                               .-container
+                               (.querySelector "#counter")
+                               .-innerHTML))
+        increase-elem    (-> ui
+                             .-container
+                             (.querySelector "#increase"))
+        reset-elem       (-> ui
+                             .-container
+                             (.querySelector "#reset"))]
     (testing "Counter initial state"
-      (let [couter-elem (-> [:#counter]
-                            sel1
-                            .-innerHTML)]
-        (is (= "0" couter-elem))))))
+      (is (= "0" (get-counter-elem))))
+
+    (testing "Counter Increase"
+      (.click rtl/fireEvent increase-elem)
+      (r/flush)
+      (is (= "1"(get-counter-elem))))
+
+    (testing "Counter Reset"
+      (.click rtl/fireEvent reset-elem)
+      (r/flush)
+      (is (= "0" (get-counter-elem))))))
