520c567f43ed1cf0582794aa215c70fd0086e2c7
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-19 07:28:29 -0300

    Fix test to use utils

--- test/sguis/workspaces/counter_test.cljs
+++ test/sguis/workspaces/counter_test.cljs
@@ -5,35 +5,34 @@
              :refer [is testing use-fixtures]]
             [nubank.workspaces.core :as ws]
             [reagent.core :as r]
-            ["@testing-library/react" :as rtl]))
-
-(use-fixtures :each
-  {:after rtl/cleanup})
+            ["@testing-library/react" :as rtl]
+            [sguis.workspaces.test-utils :as u]))
 
 (ws/deftest counter-tests
-  (let [ui               (-> [counter-ui (r/atom counter-start)]
-                             r/as-element
-                             rtl/render)
-        get-counter-elem (fn []
-                           (-> ui
+  (let [get-counter-elem (fn [comp]
+                           (-> comp
                                .-container
                                (.querySelector "#counter")
                                .-innerHTML))
-        increase-elem    (-> ui
-                             .-container
-                             (.querySelector "#increase"))
-        reset-elem       (-> ui
-                             .-container
-                             (.querySelector "#reset"))]
-    (testing "Counter initial state"
-      (is (= "0" (get-counter-elem))))
+        increase-elem    (fn [comp]
+                           (-> comp
+                               .-container
+                               (.querySelector "#increase")))
+        reset-elem       (fn [comp]
+                           (-> comp
+                               .-container
+                               (.querySelector "#reset")))]
 
-    (testing "Counter Increase"
-      (.click rtl/fireEvent increase-elem)
-      (r/flush)
-      (is (= "1" (get-counter-elem))))
+    (u/with-mounted-component [counter-ui (r/atom counter-start)]
+      (fn [comp]
+        (testing "Initial render."
+          (is (= "0" (get-counter-elem comp))))
 
-    (testing "Counter Reset"
-      (.click rtl/fireEvent reset-elem)
-      (r/flush)
-      (is (= "0" (get-counter-elem))))))
+        (testing "Increase button."
+          (u/click-element! (increase-elem comp))
+          (u/click-element! (increase-elem comp))
+          (is (= "2" (get-counter-elem comp))))
+
+        (testing "Decrease button."
+          (u/click-element! (reset-elem comp))
+          (is (= "0" (get-counter-elem comp))))))))
