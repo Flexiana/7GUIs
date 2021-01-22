af32f40629b88cdba31927fcf96a36e1648a0a8c
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-01-22 13:00:53 +0100

    Use fake timer to simulate Timer's run.

--- test/sguis/workspaces/timer_test.cljs
+++ test/sguis/workspaces/timer_test.cljs
@@ -8,32 +8,28 @@
             [reagent.core :as r]
             [sguis.workspaces.test-utils :as u]))
 
-(defn wait [milliseconds f]
-  (js/setTimeout f milliseconds))
-
 (ws/deftest
   timer-test
-  (let [reset-button #(.getByText % "Reset!")
+  (let [js-timer (u/install-timer)
+        *test-timer (r/atom {})
+        reset-button #(.getByText % "Reset!")
         label #(.getByTestId % "timer")
         input #(.getByTestId % "range")
         progress #(.getByTestId % "progress")]
     (u/with-mounted-component
-      [#(timer-ui *timer)]
+      [timer-ui *test-timer]
       (fn [comp]
         (testing "Initialization"
           (are [expected actual] (= expected actual)
-            js/HTMLButtonElement (type (reset-button comp))
-            js/HTMLDivElement (type (label comp))
-            "0s" (.-innerHTML (label comp))
-            "1" (.-value (input comp))
-            "1" (.-min (input comp))
-            "100" (.-max (input comp))
-            nil (.-width (progress comp))))
+                                 js/HTMLButtonElement (type (reset-button comp))
+                                 js/HTMLDivElement (type (label comp))
+                                 "0s" (.-innerHTML (label comp))
+                                 "1" (.-value (input comp))
+                                 "1" (.-min (input comp))
+                                 "100" (.-max (input comp))
+                                 nil (.-width (progress comp))))
         (testing "Run"
-          (reset! *timer {})
-          (u/change-element! (input comp) {:target {:value 100}})
-          (is (= "100" (.-value (input comp))))
-          ;(wait 10000 #(js/alert "most"))
-          (is (= "100" (.getPropertyValue (.-style (progress comp)) "width"))))))))
-          ;(is (some? (wait 10 (.-value (progress comp)))))
-          ;(is (= 100 (wait 100 (.-valueAsNumber (input comp))))))))))
+          (u/input-element! (input comp) {:target {:value "10"}})
+          (u/tick js-timer 5000)
+          (is (= {:elapsed-time 5, :duration 10} @*test-timer))
+          (u/uninstall-timer js-timer))))))
