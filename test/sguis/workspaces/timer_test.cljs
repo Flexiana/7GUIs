14d1a8b15e3b82afb99a5b8b30f227fa42b84bb6
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-01-22 13:10:15 +0100

    Fails, UI doesn't updating in sync with the atom's change

--- test/sguis/workspaces/timer_test.cljs
+++ test/sguis/workspaces/timer_test.cljs
@@ -8,10 +8,13 @@
             [reagent.core :as r]
             [sguis.workspaces.test-utils :as u]))
 
+(defn wait [milliseconds f]
+  (js/setTimeout f milliseconds))
+
 (ws/deftest
   timer-test
-  (let [js-timer (u/install-timer)
-        *test-timer (r/atom {})
+  (let [fake-timer (u/install-timer)
+        *test-timer (atom {})
         reset-button #(.getByText % "Reset!")
         label #(.getByTestId % "timer")
         input #(.getByTestId % "range")
@@ -27,9 +30,11 @@
                                  "1" (.-value (input comp))
                                  "1" (.-min (input comp))
                                  "100" (.-max (input comp))
-                                 nil (.-width (progress comp))))
+                                 "0%" (.getPropertyValue (.-style (progress comp)) "width")))
         (testing "Run"
           (u/input-element! (input comp) {:target {:value "10"}})
-          (u/tick js-timer 5000)
+          (u/tick fake-timer 5000)
           (is (= {:elapsed-time 5, :duration 10} @*test-timer))
-          (u/uninstall-timer js-timer))))))
+          (is (= "5s" (.-innerHTML (label comp))))
+          (is (= "50%" (.getPropertyValue (.-style (progress comp)) "width")))
+          (u/uninstall-timer fake-timer))))))
