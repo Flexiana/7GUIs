69f6ffe3115b88ae35f9b48686edadeb82c49a6e
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-22 16:02:17 -0300

    Add test for Reset Button

--- test/sguis/workspaces/timer_test.cljs
+++ test/sguis/workspaces/timer_test.cljs
@@ -1,5 +1,6 @@
 (ns sguis.workspaces.timer-test
-  (:require [sguis.workspaces.timer :refer [timer-ui]]
+  (:require [sguis.workspaces.timer :refer [timer-ui
+                                            timer-start]]
             [cljs.test :as t
              :include-macros true
              :refer [are is testing]]
@@ -7,15 +8,18 @@
             [reagent.core :as r]
             [sguis.workspaces.test-utils :as u]))
 
+(defn wait [milliseconds f]
+  (js/setTimeout f milliseconds))
+
 (ws/deftest timer-test
   (let [fake-timer   (u/install-timer)
-        *test-timer  (r/atom {})
+        *test-timer  (r/atom timer-start)
         reset-button #(.getByText % "Reset!")
         label        #(.getByTestId % "timer")
         input        #(.getByTestId % "range")
         progress     #(.getByTestId % "progress")]
     (u/with-mounted-component
-      [timer-ui]
+      [timer-ui *test-timer]
       (fn [comp]
         (testing "Initialization"
           (are [expected actual] (= expected actual)
@@ -28,9 +32,19 @@
             "0%"                 (.getPropertyValue (.-style (progress comp)) "width")))
         (testing "Run"
           (u/input-element! (input comp) {:target {:value "10"}})
+          (is (= "0%" (.getPropertyValue (.-style (progress comp)) "width")))
           (u/tick fake-timer 5000)
           (is (= {:elapsed-time 5, :duration 10} @*test-timer))
           (r/flush)
           (is (= "5s" (.-innerHTML (label comp))))
-          (is (= "50%" (.getPropertyValue (.-style (progress comp)) "width")))
-          (u/uninstall-timer fake-timer))))))
+          (is (= "50%" (.getPropertyValue (.-style (progress comp)) "width"))))
+
+        (testing "Reset button"
+          (u/click-element! (reset-button comp))
+          (is (= {:elapsed-time 0, :duration 10}  @*test-timer))
+          (u/tick fake-timer 10000)
+          (is (= {:elapsed-time 10 :duration 10} @*test-timer))
+          (r/flush)
+          (is (= "" (.getPropertyValue (.-style (progress comp)) "width"))))
+
+        (u/uninstall-timer fake-timer)))))
