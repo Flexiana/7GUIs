1f2c6296284329770694981816c3d93b4ab15f4d
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-01-23 10:45:08 +0100

    use (are ... ) in cell-test

--- test/sguis/workspaces/cells_test.cljs
+++ test/sguis/workspaces/cells_test.cljs
@@ -2,16 +2,17 @@
   (:require [sguis.workspaces.cells :refer [eval-cell]]
             [cljs.test :as t
              :include-macros true
-             :refer [is]]
+             :refer [are]]
             [nubank.workspaces.core :as ws]))
 
 (ws/deftest eval-cell-test
-  (is (= "10" (eval-cell {:cells {:A2 "2" :B8 "8"}} "Sum of A2:B8 =")))
-  (is (= "0" (eval-cell {} "Sum of A2:B8 =")))
-  (is (= "4" (eval-cell {} "Sum of 4 and A2:B8 =")))
-  (is (= "3" (eval-cell {} "Add 1 and 2 =")))
-  (is (= "NaN" (eval-cell {} "Div of B5 and C5 =")))
-  (is (= "a" (eval-cell {} "a")))
-  (is (= "20" (eval-cell {:cells {:A7 "10"
-                                  :G0 "10"}} "Add A7 and G0 =")))
-  (is (= "" (eval-cell {} nil))))
+  (are [expected actual] (= expected actual)
+    "10" (eval-cell {:cells {:A2 "2" :B8 "8"}} "Sum of A2:B8 =")
+    "0" (eval-cell {} "Sum of A2:B8 =")
+    "4" (eval-cell {} "Sum of 4 and A2:B8 =")
+    "3" (eval-cell {} "Add 1 and 2 =")
+    "NaN" (eval-cell {} "Div of B5 and C5 =")
+    "a" (eval-cell {} "a")
+    "20" (eval-cell {:cells {:A7 "10"
+                             :G0 "10"}} "Add A7 and G0 =")
+    "" (eval-cell {} nil)))
