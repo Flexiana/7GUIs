862af0218bc9584f1a093fa943f12997c9161089
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-26 09:57:22 -0300

    Add some helpers to facilitate tests

--- test/sguis/workspaces/crud_test.cljs
+++ test/sguis/workspaces/crud_test.cljs
@@ -5,17 +5,22 @@
              :refer [are is testing]]
             [nubank.workspaces.core :as ws]
             [sguis.workspaces.test-utils :as u]
-            [reagent.core :as r]))
+            [reagent.core :as r]
+            [clojure.string :as str]))
+
+(defn texts-on-field [field]
+  (mapv #(.-innerText %) (.-children field)))
 
 (ws/deftest crud-tests
-  (let [filter-field  #(.getByTestId % "filter")
-        name-field    #(.getByTestId % "Name:")
-        surname-field #(.getByTestId % "Surname:")
-        create-button #(.getByText % "create")
-        update-button #(.getByText % "update")
-        delete-button #(.getByText % "delete")
-        person-list   #(.getByTestId % "person-list")
-        *crud         (r/atom crud-start)]
+  (let [filter-field      #(.getByTestId % "filter")
+        name-field        #(.getByTestId % "Name:")
+        surname-field     #(.getByTestId % "Surname:")
+        create-button     #(.getByText % "create")
+        update-button     #(.getByText % "update")
+        delete-button     #(.getByText % "delete")
+        person-list       #(.getByTestId % "person-list")
+        first-person-list #(-> % person-list .-children first)
+        *crud             (r/atom crud-start)]
     (u/with-mounted-component
       [crud-ui *crud]
       (fn [comp]
@@ -38,7 +43,7 @@
           (u/input-element! (surname-field comp) "Doe")
           (is (= "Doe" (.-value (surname-field comp))))
           (u/click-element! (create-button comp))
-          (is (= (mapv #(.-innerHTML %) (.-children (person-list comp))) '("Doe, John"))))
+          (is (= (texts-on-field (person-list comp)) ["Doe, John"])))
 
         (testing "Update person"
           (reset! *crud crud-start)
@@ -47,12 +52,12 @@
           (u/click-element! (create-button comp))
           (is (empty? (.-value (name-field comp))))
           (is (empty? (.-value (surname-field comp))))
-          (u/click-element! (-> (person-list comp) .-children first))
+          (u/click-element! (first-person-list comp))
           (is (= "John" (.-value (name-field comp))))
           (is (= "Doe" (.-value (surname-field comp))))
           (u/input-element! (name-field comp) "Jane")
           (u/click-element! (update-button comp))
-          (is (= (mapv #(.-innerHTML %) (.-children (person-list comp))) '("Doe, Jane"))))
+          (is (= (texts-on-field (person-list comp)) ["Doe, Jane"])))
 
         (testing "Filtering"
           (reset! *crud crud-start)
@@ -63,7 +68,7 @@
           (u/input-element! (surname-field comp) "Foe")
           (u/click-element! (create-button comp))
           (u/input-element! (filter-field comp) "F")
-          (is (= (mapv #(.-innerHTML %) (.-children (person-list comp))) '("Foe, John"))))
+          (is (= (texts-on-field (person-list comp)) ["Foe, John"])))
 
         (testing "Delete person"
           (reset! *crud crud-start)
@@ -73,9 +78,9 @@
           (u/input-element! (name-field comp) "John")
           (u/input-element! (surname-field comp) "Foe")
           (u/click-element! (create-button comp))
-          (u/click-element! (-> (person-list comp) .-children first))
+          (u/click-element! (first-person-list comp))
           (u/click-element! (delete-button comp))
-          (is (= (mapv #(.-innerHTML %) (.-children (person-list comp))) '("Foe, John"))))
+          (is (= (texts-on-field (person-list comp)) ["Foe, John"])))
 
         (testing "State"
           (reset! *crud crud-start)
@@ -89,13 +94,13 @@
             nil                                       (:name-insertion @*crud)
             nil                                       (:surname-insertion @*crud)
             {0 {:id 0, :name "John", :surname "Doe"}} (:person/by-id @*crud))
-          (u/click-element! (-> (person-list comp) .-children first))
+          (u/click-element! (first-person-list comp))
           (are [expected actual] (= expected actual)
             "John" (:name-insertion @*crud)
             "Doe"  (:surname-insertion @*crud))
           (u/input-element! (surname-field comp) "Foe")
           (u/click-element! (update-button comp))
-          (u/click-element! (-> (person-list comp) .-children first))
+          (u/click-element! (first-person-list comp))
           (are [expected actual] (= expected actual)
             "John" (:name-insertion @*crud)
             "Foe"  (:surname-insertion @*crud))
