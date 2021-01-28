4a4e3a6a0ed1c780e9b0718c51d889040b50507a
Ian Fernandez <ian.fernandez@flexiana.com>
2021-01-28 10:21:28 -0300

    Fix Counter test

--- test/sguis/workspaces/counter_test.cljs
+++ test/sguis/workspaces/counter_test.cljs
@@ -9,13 +9,14 @@
 
 (ws/deftest counter-tests
   (let [counter-value #(-> % (.getByTestId "counter-value") .-value)
-        count-btn #(.getByText % #"(?i)count")]
-    (u/with-mounted-component [counter-ui]
-                              (fn [comp]
-                                (testing "Initial render."
-                                  (is (= "0" (counter-value comp))))
+        count-btn #(.getByText % "Count")]
+    (u/with-mounted-component
+      [counter-ui]
+      (fn [comp]
+        (testing "Initial render."
+          (is (= "0" (counter-value comp))))
 
-                                (testing "Count button."
-                                  (u/click-element! (count-btn comp))
-                                  (u/click-element! (count-btn comp))
-                                  (is (= "2" (counter-value comp))))))))
+        (testing "Count button."
+          (u/click-element! (count-btn comp))
+          (u/click-element! (count-btn comp))
+          (is (= "2" (counter-value comp))))))))
