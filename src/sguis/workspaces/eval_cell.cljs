3f1d7ca570cd3b505c8cbc74d1011843c23495e6
Ian Fernandez <ian.fernandez@flexiana.com>
2021-02-02 09:10:09 -0300

    Organize test per case

--- src/sguis/workspaces/eval_cell.cljs
+++ src/sguis/workspaces/eval_cell.cljs
@@ -50,25 +50,32 @@
                           :raw-ast '(+ 1 2)
                           :ast     '(+ 1 2)
                           :output  3}
+                     ;; simple text
+                     :A3 {:input   "abc"
+                          :raw-ast "abc"
+                          :ast     "abc"
+                          :output  "abc"}
                      ;; simple ref
-                     :A3 {:input   "A1 ="
+                     :A4 {:input   "A1 ="
                           :raw-ast :A1
                           :ast     1
                           :output  1}
                      ;; op with ref
-                     :A4 {:input   "=add(A1,A2)"
+                     :A5 {:input   "= add (A1,A2)"
                           :raw-ast '(+ :A1 :A2)
                           :ast     '(+ 1 2)
-                          :output  3}
-                     ;; simple text
-                     :A5 {:input   "abc"
-                          :raw-ast "abc"
-                          :ast     "abc"
-                          :output  "abc"}}}]
+                          :output  3}}}]
     (is (= 1 (input->raw-ast "1")))
-    (is (= "abc" (input->raw-ast "abc")))
-    (is (= '(+ :A1 :A2) (input->raw-ast "= add (A1,A2)")))
-    (is (= '(+ 1 2) (input->raw-ast "= add (1,2)")))
     (is (= 1 (raw-ast->ast env 1)))
+
     (is (= 1 (raw-ast->ast env :A1)))
+
+    (is (= "abc" (input->raw-ast "abc")))
+    (is (= "abc" (raw-ast->ast env "abc")))
+
+    (is (= '(+ 1 2) (input->raw-ast "= add (1,2)")))
+    (is (= '(+ 1 2) (raw-ast->ast env '(+ 1 2))))
+
+    (is (= '(+ :A1 :A2) (input->raw-ast "= add (A1,A2)")))
+    (is (= '(+ 1 3) (raw-ast->ast env '(+ :A1 :A2))))
     (is (= '(+ 1 2) (raw-ast->ast env '(+ :A1 2))))))
