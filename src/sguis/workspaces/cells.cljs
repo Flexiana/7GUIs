94d6c1b789bb5e0fd2eb55e3e6d65618b223eaf3
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-02-03 16:01:17 +0100

    Alternate evaluation of cells, working commit

--- src/sguis/workspaces/cells.cljs
+++ src/sguis/workspaces/cells.cljs
@@ -92,7 +92,7 @@
     (get chain s) "Circular dependency found!"
     (nil? s) ""
     (valid/numeric? s) s
-    (is-op? s) (get kw->op (keyword s))
+    (is-op? (str/lower-case s)) (get kw->op (keyword (str/lower-case s)))
     (can-parse-numeric? s) (js/parseFloat s)
     (is-cell? env s) (recur (assoc env :chain (conj chain s))
                        (->> s
@@ -140,20 +140,190 @@
   (.preventDefault event)
   (swap! *state
     #(-> %
-         (assoc-in [:cells cell-id]
-           (if
-             (str/ends-with? edition "=")
-             (str/lower-case edition)
-             edition))
+         (assoc-in [:cells cell-id :input]
+           edition)
          (dissoc :focused-cell :edition))))
 
 (defn change-cell!
   [*state event]
   (swap! *state assoc :edition (.. event -target -value)))
 
+(defn expression?
+  [x]
+  (and (string? x)
+       (re-matches #"=\s*(\w+)\s*((\((.*)\)))*" x)))
+
+(defn extract
+  [input]
+  (let [[_ c r] (re-matches #"([A-Z]+)(\d+)" input)]
+    [c (js/parseFloat r)]))
+
+(defn single-cell?
+  [input]
+  (and
+    (string? input)
+    (re-matches #"\s*([A-Z]+)(\d+)\s*" input)))
+
+(defn multi-cell?
+  [input]
+  (and (string? input)
+       (let [splices (str/split input #":")]
+         (every? single-cell? splices))))
+
+(defn operand?
+  [x]
+  (and (string? x)
+       (get (into #{} (map name (keys kw->op))) (str/lower-case x))))
+
+(defmulti parse :type)
+
+(defn seq-cell?
+  [input]
+  (every? single-cell? (str/split input ",")))
+
+(defn reader
+  [{:keys [cells chain] :or {chain #{}} :as env} id]
+  (let [input (get-in cells [(keyword id) :input])
+        typ   (cond
+                (get chain id) :cycle-error
+                (nil? input) :nil
+                (valid/numeric? input) :float
+                (can-parse-numeric? input) :parsable-float
+                (expression? input) :expression
+                (single-cell? input) :cell
+                (seq-cell? input) :seq-cell
+                (multi-cell? input) :multi-cell
+                (operand? input) :operand
+                :else :string)]
+    #(parse {:input input :id id :type typ :env env})))
+
+(defmethod parse :cycle-error [x]
+  (assoc x :error (str "Cyclic dependency found " (:id x))))
+
+(defmethod parse :float [x]
+  (assoc x :output (:input x)))
+
+(defmethod parse :nil [x]
+  (assoc x :output ""))
+
+(defmethod parse :parsable-float [x]
+  (assoc x :output (js/parseFloat (:input x))))
+
+(defmethod parse :cell [{:keys [env input id]}]
+  (let [chain (get env :chain #{})]
+    #(reader (assoc env :chain (conj chain id)) input)))
+
+(defn ->range
+  [x y]
+  [(min x y) (inc (max x y))])
+
+(defn multi->cells
+  [input]
+  (let [[f l] (str/split input ":")
+        [fc fr] (extract f)
+        [lc lr] (extract l)]
+    (for [c (apply range (->range (.charCodeAt fc) (.charCodeAt lc)))
+          r (apply range (->range fr lr))]
+      (str (char c) r))))
+
+(defn parse-multiple
+  [env cells]
+  (for [cell cells]
+    (trampoline parse (assoc env :type :cell :input cell))))
+
+(defmethod parse :multi-cell [{:keys [input] :as x}]
+  (let [cell-range (multi->cells input)]
+    (parse-multiple x cell-range)))
+
+(defmethod parse :seq-cell [{:keys [input] :as x}]
+  (let [cell-range (str/split input ",")]
+    (parse-multiple x cell-range)))
+
+(defmethod parse :string [x]
+  (assoc x :output (:input x)))
+
+(defmethod parse :operand [{:keys [input] :as x}]
+  (assoc x :output (get kw->op (keyword (str/lower-case input)))))
+
+(defmethod parse :expression [{:keys [env input]}]
+  (let [[_ op _ _ par] (re-matches #"=\s*(\w+)\s*((\((.*)\)))*" input)
+        op-cell    (keyword (gensym))
+        operand    (trampoline (reader (assoc-in env [:cells op-cell :input] op) op-cell))
+        tmp-cell   (keyword (gensym))
+        parameters (str/replace par #"=\s*(\w+)\s*((\((.*)\)))*"
+                                #(let [p (trampoline reader (assoc-in env [:cells tmp-cell :input] (first %1)) tmp-cell)]
+                                   (map :output p)))]
+
+    (->> operand
+         (conj (for [p (str/split parameters ",")
+                     :let [tmp-cell (keyword (gensym))]]
+                 (trampoline (reader (assoc-in env [:cells tmp-cell :input] p) tmp-cell))))
+         flatten)))
+
+(defn render
+  [exp]
+  (cond
+    (:error exp) (:error exp)
+    (and (seq? exp) (some :error exp)) (str/join ", " (mapv :error (filter :error exp)))
+    (and (seq? exp) (= :operand (get (first exp) :type))) (eval-string (str (map #(if (= :string  (:type %))
+                                                                                    (eval-string (:output %))
+                                                                                    (:output %)) exp)))
+    (seq? exp) (str/join ", " (mapv :output exp))
+    :else (:output exp)))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "1"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A2 {:input "A1"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A1"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A2:A3"}
+                                     :A2 {:input "2"}
+                                     :A3 {:input "3"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A2,A3"}
+                                     :A2 {:input "2"}
+                                     :A3 {:input "3"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A1:A3"}
+                                     :A2 {:input "2"}
+                                     :A3 {:input "A3"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A1,A2"}
+                                     :A2 {:input "2"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "= Add (A2:A5)"}
+                                     :A3 {:input "5"}
+                                     :A2 {:input "6"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "= Add (A3,= mul (2,A2))"}
+                                     :A3 {:input "5"}
+                                     :A2 {:input "6"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "A3:A2"}
+                                     :A3 {:input "5"}
+                                     :A2 {:input "6"}}} "A1")))
+
+(render (trampoline (reader {:chain #{}
+                             :cells {:A1 {:input "= add (A2,3)"}
+                                     :A2 {:input "2"}}} "A1")))
+
 (defn coll-fn
   [{:keys [focused-cell cells] :as env}
-   {:keys [focus-cell! submit-cell! change-cell!]} cell-width l  c]
+   {:keys [focus-cell! submit-cell! change-cell!]} cell-width l c]
   (let [cell-id (keyword (str c l))]
     ^{:key cell-id}
     [:td {:style           (light-border-style cell-width)
@@ -170,7 +340,7 @@
                  :auto-focus    true
                  :default-value (get cells cell-id)
                  :on-change     (partial change-cell!)}]]
-       (eval-cell env (get cells cell-id)))]))
+       (render (trampoline (reader env cell-id))))]))
 
 #_:clj-kondo/ignore
 
